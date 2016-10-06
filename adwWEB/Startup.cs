072cf5a4af45244286accd0e4266fb69c8067072
SLAWEK\sw <swdowia1@gazeta.pl>
2016-10-06 20:20:11 +0200

    start

---
+++ adwWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(adwWEB.Startup))]
+namespace adwWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ adwWEB/ViewModel/ProductViewList.cs
@@ -0,0 +1,11 @@
+﻿namespace adwWEB.ViewModel
+{
+    public class ProductViewList
+    {
+        public int Id { get; set; }
+        public string NameProduct { get; internal set; }
+        public string ProductCategoryName { get; internal set; }
+        public int? ProductSubcategory { get; internal set; }
+        public string ProductSubcategoryName { get; set; }
+    }
+}
